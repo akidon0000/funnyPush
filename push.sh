753361cab05f58b00733c86a495062393fd66e9d
Akihiro Matsuyama <a@art.jp>
2021-04-27 21:08:00 +0900

    あ

--- push.sh
+++ push.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 cd /Users/akidon/program/etc/funnyPush/
+echo ">> README.md"
 echo "." >> README.md
+echo "git add README.md"
 git add README.md
+echo "git commit -m '.'"
 git commit -m '.'
-git push --force
+echo "git push --force"
+#git remote set-url origin git@github.com:akidon0000/repo.git
+git push origin master
+#git remote set-url origin git@github.com:akidon0000/repo.git
+#git push --force
 echo "ノルマ達成"
